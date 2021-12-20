5d2510c6850240bd09bdc4f29456de4a1ee38a47
zlaval <zlaval@gmail.com>
2021-12-20 14:29:51 +0100

    add fail test

---
+++ src/test/kotlin/com/zlrx/DummyTestCase.kt
@@ -0,0 +1,21 @@
+package com.zlrx
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class DummyTestCase {
+
+    @Test
+    @DisplayName("Should return true")
+    fun firstTest() {
+        Assertions.assertTrue(true)
+    }
+
+    @Test
+    @DisplayName("Should return false")
+    fun secondTest() {
+        Assertions.assertTrue(false)
+    }
+
+}
